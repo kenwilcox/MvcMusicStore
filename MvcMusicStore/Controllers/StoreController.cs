ed3ccf43bf01307564c867f900e99aa8d5a82427
Ken Wilcox <wilcoxk@gmail.com>
2015-03-17 14:37:29 -0600

    added Store.Index View

--- MvcMusicStore/Controllers/StoreController.cs
+++ MvcMusicStore/Controllers/StoreController.cs
@@ -3,14 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MvcMusicStore.ViewModels;
 
 namespace MvcMusicStore.Controllers
 {
   public class StoreController : Controller
   {
-    public string Index()
+    public ActionResult Index()
     {
-      return "Hello from Store.Index()";
+      // Create a list of genres
+      var genres = new List<string> {"Rock", "Jazz", "Country", "Pop", "Disco"};
+
+      // Create our view model
+      var viewModel = new StoreIndexViewModel
+      {
+        NumberOfGenres = genres.Count(),
+        Genres = genres
+      };
+
+      return this.View(viewModel);
     }
 
     // GET: /Store/Browse?genre=Disco

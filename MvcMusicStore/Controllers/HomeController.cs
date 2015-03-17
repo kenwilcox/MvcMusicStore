24ac6e6110ce247f6d3a35c9c8fc99b249f5ddb1
Ken Wilcox <wilcoxk@gmail.com>
2015-03-17 14:20:44 -0600

    added Home.Index View

--- MvcMusicStore/Controllers/HomeController.cs
+++ MvcMusicStore/Controllers/HomeController.cs
@@ -8,9 +8,9 @@ namespace MvcMusicStore.Controllers
 {
   public class HomeController : Controller
   {
-    public string Index()
+    public ActionResult Index()
     {
-      return "Hello from Home";
+      return this.View();
     }
 
   }
